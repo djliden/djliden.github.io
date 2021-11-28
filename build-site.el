8b7292b6bfc9e65ee04f20b82e0262e57cf2362f
Daniel Liden <dliden@dliden-mbp.local>
2021-11-27 21:09:28 -0800

    initial commit

---
+++ build-site.el
@@ -0,0 +1,40 @@
+;; packages
+(require 'package)
+(setq package-user-dir (expand-file-name "./.packages"))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+;; Initialize the package system
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Install dependencies
+(package-install 'htmlize)
+
+;; Load publishing system
+(require 'ox-publish)
+(message "Build Complete!")
+
+;; define publishing project
+(setq org-publish-project-alist
+      (list
+       (list "org-site:main"
+             :recursive t
+             :base-directory "./content"
+             :publishing-directory "./public"
+             :publishing-function 'org-html-publish-to-html
+             :with-author nil
+             :with-creator t
+             :with-toc t
+             :section-numbers nil
+             :time-stamp-file nil
+             )))
+
+;; additional settings
+ (setq org-html-validation-link nil)            ;; Don't show validation link
+;;       org-html-head-include-scripts nil       ;; Use our own scripts
+;;       org-html-head-include-default-style nil ;; Use our own styles
+;;       org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
+;; generate site output
+(org-publish-all t)
