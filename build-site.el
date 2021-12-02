3878d5ac49b3f4b15c55fc3afd9224d1bb32de4d
Daniel Liden <djliden91@gmail.com>
2021-12-02 15:27:15 -0800

    add post preview functionality and post archive link

--- build-site.el
+++ build-site.el
@@ -19,6 +19,7 @@
 ;;;; Get Preview
 
 ;; modify with an "if error skip" logic
+;; still need conditional
 (defun my/get-preview (file)
   "get preview text from a file
 
@@ -27,11 +28,12 @@ https://ogbe.net/blog/blogging_with_org.html"
   (with-temp-buffer
     (insert-file-contents file)
     (goto-char (point-min))
-    (let ((beg (+ 1 (re-search-forward "^#\\+BEGIN_PREVIEW$")))
-          (end (progn (re-search-forward "^#\\+END_PREVIEW$")
-                      (match-beginning 0))))
-      (buffer-substring beg end))))
-
+    (when (re-search-forward "^#\\+BEGIN_PREVIEW$" nil 1)
+      (goto-char (point-min))
+      (let ((beg (+ 1 (re-search-forward "^#\\+BEGIN_PREVIEW$" nil 1)))
+            (end (progn (re-search-forward "^#\\+END_PREVIEW$" nil 1)
+                        (match-beginning 0))))
+        (buffer-substring beg end)))))
 
 ;;;; Format sitemap entries
 (defun my/sitemap-entry (entry style project)
@@ -39,6 +41,7 @@ https://ogbe.net/blog/blogging_with_org.html"
 
 See code here for foundation:
 https://loomcom.com/blog/0110_emacs_blogging_for_fun_and_profit.html"
+  (print entry)
   (when (not (directory-name-p entry)) ; when not a directory
     (format (string-join
              '("*[[file:%s][%s]]\n"
@@ -50,28 +53,43 @@ https://loomcom.com/blog/0110_emacs_blogging_for_fun_and_profit.html"
             entry
             (org-publish-find-title entry project)
             (format-time-string "%A, %B %_d %Y at %l:%M %p %Z" (org-publish-find-date entry project))
-            (let ((preview (my/get-preview entry)))
-              (insert preview)))))    
+            ;(let ((preview (my/get-preview (concat "content/" entry))))
+             ; (insert preview))
+            )))    
 ;;;; Format Sitemap
 ;; modify this one! (if necessary)
 (defun my/org-publish-org-sitemap (title list)
   "Sitemap generation function."
-  (concat "#+TITLE: Sitemap\n\n")
+  (concat "#+TITLE: Sitemap\n\n#+OPTIONS: toc:nil")
   (org-list-to-subtree list))
 
 ;; modify this one!
 (defun my/org-publish-org-sitemap-format (entry style project)
   "Custom sitemap entry formatting: add date"
   (cond ((not (directory-name-p entry))
-         (format "[[file:%s][(%s) %s]]"
+         (let ((preview (if (my/get-preview (concat "content/" entry))
+                            (my/get-preview (concat "content/" entry))
+                          "(No preview)")))
+         (format "[[file:%s][(%s) %s]]\n%s"
                  entry
                  (format-time-string "%Y-%m-%d"
                                      (org-publish-find-date entry project))
-                 (org-publish-find-title entry project)))
+                 (org-publish-find-title entry project)
+                 preview)))
+                 ;;(my/get-preview (concat "content/" entry))))
         ((eq style 'tree)
          ;; Return only last subdir.
+         ;; ends up as a headline at higher level than the posts
+         ;; it contains
          (file-name-nondirectory (directory-file-name entry)))
         (t entry)))
+
+;;;;; Notes about Sitemap Formatting
+
+;; (unordered ("[[file:index.org][(2021-12-01) Daniel Liden's Home Page]]") ("[[file:about.org][(2021-11-28) About Me]]") ("posts" (unordered ("[[file:posts/test1.org][(2021-11-28) Resources]]") ("[[file:posts/test2.org][(2021-11-28) Another Post]]"))))
+
+;; this ^ is the list produced. We can see the tree structure. (unordered (posts)) at the top
+;; level and then ("posts" (unordered (posts))) at the lower level.
 
 ;;; define publishing project
 (setq org-publish-project-alist
@@ -88,11 +106,8 @@ https://loomcom.com/blog/0110_emacs_blogging_for_fun_and_profit.html"
              :time-stamp-file nil
              :auto-sitemap t
              :sitemap-title nil;"Daniel Liden's Blog"
-             ;:sitemap-function 'loomcom/sitemap
              :sitemap-function 'my/org-publish-org-sitemap
              :sitemap-sort-files 'anti-chronologically
-             ;:sitemap-format-entry 'my/test-format
-             ;:sitemap-format-entry 'my/sitemap-entry
              :sitemap-format-entry 'my/org-publish-org-sitemap-format
              :sitemap-filename "sitemap.org"
              :sitemap-style 'tree
